411acba65df059d74b4877c0dcb2fa7da0ddbf0f
pointcache <antonov.3d@gmail.com>
2017-02-21 20:19:39 -0500

    update

--- FbxImporter.cs
+++ FbxImporter.cs
@@ -15,7 +15,7 @@ public class FbxImporter : MonoBehaviour {
     [Serializable]
     public class FBXNode {
         public string Name;
-        public string[] properties;
+        public List<string> properties;
         public List<FBXNode> nodes = new List<FBXNode>();
     }
 
@@ -36,7 +36,7 @@ public class FbxImporter : MonoBehaviour {
                 while (!sr.EndOfStream) {
                     string line = sr.ReadLine();
                     //Skip comments and empty lines
-                    if (line.Length == 0 || line[0] == ';')
+                    if (line.Length == 0 || isComment(line))
                         continue;
                     //detect node start
                     if (line.Contains(":")) {
@@ -48,13 +48,15 @@ public class FbxImporter : MonoBehaviour {
         catch (Exception e) {
             Debug.LogError("The file could not be read:");
             Debug.LogError(e.Message);
+            Debug.LogError(e.StackTrace);
         }
 
+        file.Name = Path.GetFileNameWithoutExtension(path);
         return file;
     }
 
     static FBXNode new_node(StreamReader sr, string line) {
-        if (!line.Contains(":"))
+        if (!line.Contains(":") || isComment(line))
             return null;
 
         FBXNode node = new FBXNode();
@@ -64,7 +66,7 @@ public class FbxImporter : MonoBehaviour {
             while (!next.Contains("}")) {
                 next = sr.ReadLine();
                 if (!next.Contains("}"))
-                    node.nodes.Add(new_node(sr, next));
+                    addNode(node, new_node(sr, next));
             }
         }
 
@@ -73,11 +75,53 @@ public class FbxImporter : MonoBehaviour {
 
         string props = line.Remove(0, split1[0].Length + 1);
         props = props.Remove(props.Length - 1, 1);
-
         string[] propsSplit = props.Split(',');
-        if (propsSplit.Length > 0)
-            node.properties = propsSplit;
+        if (propsSplit.Length > 0) {
+
+            for (int i = 0; i < propsSplit.Length; i++) {
+                if (IsNullOrWhiteSpace(propsSplit[i]))
+                    continue;
+
+                if (node.properties == null)
+                    node.properties = new List<string>();
+
+                if (propsSplit[i][0] == ' ')
+                    node.properties.Add(propsSplit[i].Remove(0, 1));
+                else
+                    node.properties.Add(propsSplit[i]);
+            }
+        }
 
         return node;
+    }
+
+    static void addNode(FBXNode parent, FBXNode child) {
+        if (child == null)
+            return;
+        parent.nodes.Add(child);
+    }
+
+    static bool isComment(string line) {
+        bool result = false;
+        for (int i = 0; i < line.Length; i++) {
+            if (line[i] == ' ' || line[i] == '\t')
+                continue;
+            if (line[i] == ';') {
+                result = true;
+                break;
+            } else
+                break;
+        }
+        return result;
+    }
+
+    public static bool IsNullOrWhiteSpace(String value) {
+        if (value == null) return true;
+
+        for (int i = 0; i < value.Length; i++) {
+            if (!Char.IsWhiteSpace(value[i])) return false;
+        }
+
+        return true;
     }
 }
